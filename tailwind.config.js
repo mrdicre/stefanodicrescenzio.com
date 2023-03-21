9f8aa79ef3dbbbdb23d29317fbbb88775af0bf45
Stefano <stefano.dicre@gmail.com>
2023-03-21 14:25:44 +0000

    BACK #1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "neon": "#CCFF00",
+        "forest": "#163832",
+      },
+    },
+  },
+  plugins: [],
+}
